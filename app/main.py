f73f68b64c2ec45155b13f7137b749b48f8b67c0
SangappaMallad <sangappamallad0067@gmail.com>
2026-06-08 22:21:46 +0530

    testing for ci/cd pipeline here

--- app/main.py
+++ app/main.py
@@ -1,45 +1,8 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-
-from app.database import engine, SessionLocal
-from app.models import Product
-from app.schemas import ProductCreate
-
-from app.database import Base
-
-Base.metadata.create_all(bind=engine)
+from fastapi import FastAPI
 
 app = FastAPI()
 
+@app.get("/")
+def health_check():
+    return {"message": "API is running on Live Server"}
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-@app.post("/products")
-def create_product(
-    product: ProductCreate,
-    db: Session = Depends(get_db)
-):
-
-    new_product = Product(
-        name=product.name,
-        price=product.price
-    )
-
-    db.add(new_product)
-    db.commit()
-    db.refresh(new_product)
-
-    return new_product
-
-
-@app.get("/products")
-def get_products(
-    db: Session = Depends(get_db)
-):
-    return db.query(Product).all()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,72 +1,12 @@
-import pytest
+# tests/test_main.py
+
+
 from fastapi.testclient import TestClient
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-from app.database import Base
-from app.main import app, get_db   # adjust import if file name differs
-
-from app.schemas import ProductCreate
-
-
-# Use in-memory SQLite for tests
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
-
-TestingSessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-# Create test DB tables
-Base.metadata.create_all(bind=engine)
-
-
-# Dependency override
-def override_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-
-app.dependency_overrides[get_db] = override_get_db
+from app.main import app
 
 client = TestClient(app)
 
-
-# -----------------------
-# TEST: CREATE PRODUCT
-# -----------------------
-def test_create_product():
-    response = client.post(
-        "/products",
-        json={
-            "name": "Laptop",
-            "price": 50000
-        }
-    )
+def test_health():
+    response = client.get("/")
 
     assert response.status_code == 200
-    data = response.json()
-
-    assert data["name"] == "Laptop"
-    assert data["price"] == 50000
-    assert "id" in data
-
-
-# -----------------------
-# TEST: GET PRODUCTS
-# -----------------------
-def test_get_products():
-    response = client.get("/products")
-
-    assert response.status_code == 200
-    assert isinstance(response.json(), list)
