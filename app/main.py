f1cd9d4029cc7b3b4cf85def69b2c42a53ca1373
SangappaMallad <sangappamallad0067@gmail.com>
2026-06-08 16:40:33 +0530

    testing for ci/cd pipeline here

--- app/main.py
+++ app/main.py
@@ -1,7 +1,48 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
+
+# Dummy database
+products = []
+
+class Product(BaseModel):
+    id: int
+    name: str
+    price: float
+
 
 @app.get("/")
 def health_check():
     return {"message": "API is running on local server"}
+
+
+# Get all products
+@app.get("/products")
+def get_products():
+    return products
+
+
+# Add product
+@app.post("/products")
+def add_product(product: Product):
+    products.append(product.dict())
+    return {
+        "message": "Product added successfully",
+        "product": product
+    }
+
+
+# Delete product by id
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int):
+
+    for product in products:
+        if product["id"] == product_id:
+            products.remove(product)
+            return {"message": "Product deleted successfully"}
+
+    raise HTTPException(
+        status_code=404,
+        detail="Product not found"
+    )
